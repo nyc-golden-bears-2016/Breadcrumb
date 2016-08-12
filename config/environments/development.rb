7a4f945795b33038702c88cf0cc915458e7732ff
marcomanomat <marco.manomat@gmail.com>
2016-08-12 14:22:56 -0400

    add functionality for forgot password email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,17 +29,19 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 
 config.action_mailer.delivery_method = :smtp
 
 config.action_mailer.smtp_settings = {
 address: "smtp.gmail.com",
 port: 587,
-domain: ENV["GMAIL_DOMAIN"],
+domain: "gmail",
 authentication: "plain",
 enable_starttls_auto: true,
-user_name: ENV["GMAIL_USERNAME"],
-password: ENV["GMAIL_PASSWORD"]
+user_name: "breadcrumbdbc",
+password: "dbc@1234",
+:openssl_verify_mode => 'none' 
 }
 
   config.action_mailer.perform_caching = false
