20c7c669fa6b7f7d11d1c9dfc064e3a4f43fa9b0
marcomanomat <marco.manomat@gmail.com>
2016-08-13 18:10:57 -0400

    get basic search to work on console

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,13 @@
 class Tag < ApplicationRecord
+<<<<<<< 151a128ef64dc6d850972d2b67998f679d8c39c5
   has_many :trails
   has_many :trails, through: :tag_trails
+=======
+	include PgSearch
+  multisearchable :against => [:subject]
+
+  has_and_belongs_to_many :trails
+>>>>>>> get basic search to work on console
 
   validates :subject, presence: true
 
