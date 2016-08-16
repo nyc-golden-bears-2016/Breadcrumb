8ca2a2264a96a9e30ad3dc9421f1956800e3beab
Mason Brown <mason.patrick.brown@gmail.com>
2016-08-16 05:36:26 -0400

    add basic crumb spacing logic

--- app/controllers/crumbs_controller.rb
+++ app/controllers/crumbs_controller.rb
@@ -9,7 +9,11 @@ before_action :trail_creator, only: [:show]
 
   def create
     @crumb = @trail.crumbs.new(crumb_params.merge(order_number: @trail.crumbs.length))
-    if @crumb.save
+    if invalid_crumb_spacing(@crumb)
+      byebug
+      redirect_to current_user
+      #make error handling
+    elsif @crumb.save
       redirect_to "/trails/#{@trail.id}/edit"
     else
       redirect_to current_user
@@ -53,6 +57,20 @@ before_action :trail_creator, only: [:show]
 
   def crumb_params
     params.require(:crumb).permit(:name, :description, :latitude, :longitude, :requires_answer, :answer, :order_number, :img)
+  end
+
+  def invalid_crumb_spacing(new_crumb)
+    unless @trail.crumbs.first.new_record?
+      invalid_crumbs = @trail.crumbs[0..-1].select do | crumb |
+         Haversine.distance(crumb.latitude, crumb.longitude, new_crumb.latitude, new_crumb.longitude).to_meters < 30
+      end
+      if invalid_crumbs.empty?
+        return false
+      else
+        return true
+      end
+    end
+    return false
   end
 
 end
